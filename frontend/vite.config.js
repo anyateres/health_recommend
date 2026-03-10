a04b8f5ef522e8c1caa67eef3e15a81ae8bb51c9
Hanna Tereshchenko <anyateres@gmail.com>
2026-03-10 10:52:22 +0200

    Add files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
-
-const certPath = '/home/anya/IdeaProjects/health_recommend/cert.pem'
-const keyPath = '/home/anya/IdeaProjects/health_recommend/key.pem'
-const hasCerts = fs.existsSync(certPath) && fs.existsSync(keyPath)
-
-const serverConfig = {
+var certPath = '/home/anya/IdeaProjects/health_recommend/cert.pem';
+var keyPath = '/home/anya/IdeaProjects/health_recommend/key.pem';
+var hasCerts = fs.existsSync(certPath) && fs.existsSync(keyPath);
+var serverConfig = {
     port: 5173,
     host: '0.0.0.0',
 };
-
 if (hasCerts) {
     serverConfig.https = {
         key: fs.readFileSync(keyPath),
         cert: fs.readFileSync(certPath),
     };
 }
-
 export default defineConfig({
     plugins: [react()],
     server: serverConfig,
